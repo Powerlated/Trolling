d0eb958986c5755399cd81847ef2183916fdfb91
Powerlated <La>
2015-05-10 19:50:22 -0400

    Added stuff

---
+++ src/io/github/powerlated/effects/LiftOnJump.java
@@ -0,0 +1,33 @@
+package io.github.powerlated.effects;
+
+import io.github.powerlated.Troll;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
+
+public class LiftOnJump implements Listener {
+
+	Vector v1 = new Vector(0.0D, 0.41D, 0.0D);
+	Vector v2 = new Vector(0.0D, 0.45D, 0.0D);
+
+	@EventHandler
+	public void liftOnJump(PlayerMoveEvent event) throws InterruptedException {
+		Vector v = event.getPlayer().getVelocity();
+		if ((this.v1.getY() < v.getY()) && (this.v2.getY() > v.getY())
+				&& (Troll.lift.contains(event.getPlayer()))) {
+			Location loc = event.getPlayer().getLocation();
+			Block b = loc.getBlock();
+			if ((b.getType() == Material.STATIONARY_WATER)
+					|| (b.getType() == Material.WATER)) {
+				return;
+			}
+			event.getPlayer().setVelocity(new Vector(0, 10, 0));
+			Troll.lift.remove(event.getPlayer());
+		}
+	}
+}
