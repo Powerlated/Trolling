7a6de4ecf2a25a32eddf312e2cb861748a1dd00b
Powerlated <La>
2015-05-10 23:16:20 -0400

    Added dirt punch

--- src/io/github/powerlated/Troll.java
+++ src/io/github/powerlated/Troll.java
@@ -1,5 +1,6 @@
 package io.github.powerlated;
 
+import io.github.powerlated.effects.DirtPunch;
 import io.github.powerlated.effects.LiftOnJump;
 
 import java.util.HashSet;
@@ -16,10 +17,11 @@ import org.bukkit.util.Vector;
 
 public final class Troll extends org.bukkit.plugin.java.JavaPlugin {
 	public static HashSet<Player> lift = new HashSet<Player>();
-	private Block bl;
+	public static HashSet<Player> dirtPunch = new HashSet<Player>();
 
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(new LiftOnJump(), this);
+		getServer().getPluginManager().registerEvents(new DirtPunch(), this);
 	}
 
 	public void onDisable() {
@@ -36,31 +38,53 @@ public final class Troll extends org.bukkit.plugin.java.JavaPlugin {
 
 			if (args[0].equalsIgnoreCase("anvil")) {
 				boolean cos = CheckOnlineStatus.check(sender, args, 1);
+				if (!checkLength(args, 2)) {
+					usage(sender);
+					return true;
+				}
 				if (cos == false) {
 					return true;
 				}
 				Player player = Bukkit.getServer().getPlayer(args[1]);
 				Location l = player.getLocation();
 				player.sendMessage(ChatColor.RED + "Look up!");
-				Block bl = l.getBlock();
 				l = l.add(new Vector(0, 30, 0));
 				Block b = l.getBlock();
 				b.setType(Material.ANVIL, true);
 				return true;
 			} else if (args[0].equalsIgnoreCase("hj")) {
-				if (args.length == 2) {
-					Player player = Bukkit.getPlayerExact(args[1]);
-					boolean cos = CheckOnlineStatus.check(sender, args, 1);
-					if (!cos) {
-						return true;
-					}
-					lift.add(player);
-					sender.sendMessage(ChatColor.GRAY + "The next time "
-							+ args[1] + " jumps, his character will be lifted");
+				if (!checkLength(args, 2)) {
+					return true;
 				}
+				
+				boolean cos = CheckOnlineStatus.check(sender, args, 1);
+				if (!cos) {
+					return true;
+				}
+				lift.add(Bukkit.getPlayerExact(args[1]));
+				sender.sendMessage(ChatColor.GRAY + "The next time " + args[1]
+						+ " she/he will be lifted into the air");
+
 				return true;
 			} else if (args[0].equalsIgnoreCase("dp")) {
-
+				if (args.length == 3) {
+					if (args[2] == "stop") {
+						dirtPunch.remove(Bukkit.getServer().getPlayer(args[1]));
+						sender.sendMessage(ChatColor.GRAY + "Troll stopped for " + args[1]);
+						return true;
+					}
+				}
+				if (!checkLength(args, 2)) {
+					usage(sender);
+					return true;
+				}
+				if (!CheckOnlineStatus.check(sender, args, 1)) {
+					return true;
+				}
+				sender.sendMessage(ChatColor.GRAY + "When " + args[1]
+						+ " left clicks blocks now, they will turn to dirt");
+				dirtPunch.add(Bukkit.getPlayerExact(args[1]));
+				return true;
 			}
 
 			if (args[0].equalsIgnoreCase("help")) {
@@ -91,7 +115,9 @@ public final class Troll extends org.bukkit.plugin.java.JavaPlugin {
 			+ "--- Showing help page 1 out of 1 (/troll help <page> ---\n"
 			+ ChatColor.WHITE
 			+ "hj - Makes the player jump really high, ONCE\n"
-			+ "anvil - Drops an anvil on the player\n" + "dc - Makes every single block the player clicks on drop dirt");
+			+ "anvil - Drops an anvil on the player\n" 
+			+ "dc - Makes every single block the player clicks on drop dirt\n" 
+			+ "    - parameter: stop - Stops the troll");
 
 	private void help(CommandSender sender) {
 		if (sender instanceof Player) {
@@ -99,6 +125,13 @@ public final class Troll extends org.bukkit.plugin.java.JavaPlugin {
 		} else {
 			System.out.println(helpMessage);
 		}
+	}
+
+	private boolean checkLength(String[] args, int length) {
+		if (args.length == length) {
+			return true;
+		} else
+			return false;
 	}
 
 }
--- src/io/github/powerlated/effects/DirtPunch.java
+++ src/io/github/powerlated/effects/DirtPunch.java
@@ -1,5 +1,24 @@
 package io.github.powerlated.effects;
 
-public class DirtPunch {
+import io.github.powerlated.Troll;
 
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+public class DirtPunch implements Listener {
+
+
+	@EventHandler
+	public void onPlayerInteractEvent(PlayerInteractEvent event) {
+		if ((Troll.dirtPunch.contains(event.getPlayer()) && event.getAction() == Action.LEFT_CLICK_BLOCK)) {
+			if ((event.getClickedBlock().getType() == Material.RED_ROSE)
+			   || (event.getClickedBlock().getType() == Material.YELLOW_FLOWER) || (event.getClickedBlock().getType() == Material.BEDROCK)
+			   || (event.getClickedBlock().getType() == Material.LONG_GRASS) || (event.getClickedBlock().getType() == Material.CHEST) || (event.getClickedBlock().getType() == Material.DOUBLE_PLANT)) {
+				return;
+			}
+			event.getClickedBlock().setType(Material.DIRT);
+		}
+	}
 }
